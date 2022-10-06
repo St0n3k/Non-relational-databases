bfb894fb48f0a404fada1702dbbe7ff75d2f8474
236538 Graczyk Kamil <236538@edu.p.lodz.pl>
2022-10-06 13:52:46 +0200

    Client and Rent done

--- Guesthouse/src/main/java/pl/lodz/nbd/model/Client.java
+++ Guesthouse/src/main/java/pl/lodz/nbd/model/Client.java
@@ -1,4 +1,41 @@
 package pl.lodz.nbd.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Client {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "client_id")
+    private Long clientId;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String personalId;
+
+    @Column
+    private Boolean archived;
+
+    @ManyToOne
+    @JoinColumn
+    private Address address;
+
+    public Client(String firstName, String lastName, String personalId, Boolean archived) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.personalId = personalId;
+        this.archived = archived;
+    }
 }
--- Guesthouse/src/main/java/pl/lodz/nbd/model/Rent.java
+++ Guesthouse/src/main/java/pl/lodz/nbd/model/Rent.java
@@ -1,4 +1,41 @@
 package pl.lodz.nbd.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+import pl.lodz.nbd.model.Room;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Rent {
+    @Id
+    @GeneratedValue
+    @Column(name = "rent_id")
+    private Long rentId;
+
+    @Column
+    private Date beginTime;
+
+    @Column
+    private Date endTime;
+
+    @Column
+    private Boolean board;
+
+    @Column
+    private Double finalCost;
+
+    @ManyToOne
+    @JoinColumn
+    private Client client;
+
+    @JoinColumn
+    @ManyToOne
+    private Room room;
 }
