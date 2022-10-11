ba16bda7b07cb2c93c86a36118ae2dfbb510b9fd
St0n3k <kamillokoks@gmail.com>
2022-10-11 20:19:52 +0200

    Changed updateRent to updateRentBoard and wrote new cost calculation for it.

--- Guesthouse/src/main/java/pl/lodz/nbd/manager/RentManager.java
+++ Guesthouse/src/main/java/pl/lodz/nbd/manager/RentManager.java
@@ -67,10 +67,20 @@ public class RentManager {
         return repeatableTransaction(beginTime, endTime, board, clientPersonalId, roomNumber);
     }
 
-    public Rent updateRent(Rent rent) {
+    public Rent updateRentBoard(Long rentId, boolean board) {
         try (EntityManager em = EntityManagerCreator.getEntityManager()) {
             em.getTransaction().begin();
-            Rent rentSaved = rentRepository.update(rent, em);
+            Rent rentToModify = rentRepository.getById(rentId, em);
+            rentToModify.setBoard(board);
+            double newCost = calculateTotalCost(
+                    rentToModify.getBeginTime(),
+                    rentToModify.getEndTime(),
+                    rentToModify.getRoom().getPrice(),
+                    rentToModify.isBoard(),
+                    rentToModify.getClient().getClientType()
+            );
+            rentToModify.setFinalCost(newCost);
+            Rent rentSaved = rentRepository.update(rentToModify, em);
             em.getTransaction().commit();
             return rentSaved;
         } catch (Exception e) {
